9032399a94bd10de2d0a6e25391b8df0cf2ca38f
scott1223 <cdultra2012@gmail.com>
2024-10-15 00:05:17 +0200

    change cms url in .env and global settings

--- src/lib/globals.js
+++ src/lib/globals.js
@@ -1,4 +1,4 @@
-const CMS_URL = 'https://strapi.cr.heymarketing.ru'
+const CMS_URL = 'https://strapi-dev.cr.heymarketing.ru'
 const SITE_URL = 'https://sveltekit-app.cr.heymarketing.ru/'
 
 export { CMS_URL, SITE_URL }
