e245f29bed82e9fae18194da46e7a6be61650eea
scott1223 <cdultra2012@gmail.com>
2024-10-22 11:31:37 +0200

    services grid css

--- src/routes/service/+page.svelte
+++ src/routes/service/+page.svelte
@@ -18,4 +18,52 @@
 </svelte:head>
 
 <HeroMedia heroImages1={data.mediaPage.attributes.heroImages.data} heroImages2={data.mediaPage.attributes.heroImages2.data} title={data.mediaPage.attributes.title}/>
-<MediaGrid mediaArray={data.mediaArray.data} sectionCode='/service/' />
+<div class='cdu-service-mediagrid-wrapper'>
+  <MediaGrid mediaArray={data.mediaArray.data} sectionCode='/service/' />
+</div>
+
+<style lang="scss">
+	:global(.cdu-service-mediagrid-wrapper  .media-grid.media-grid) {
+      width: 80%;
+      margin: auto;
+      min-width: 1200px;
+      flex-wrap: wrap;
+      flex-direction: row;
+    }
+
+  :global(.cdu-service-mediagrid-wrapper .media-grid-borders) {
+      display: none !important;
+  }
+
+  :global(.cdu-service-mediagrid-wrapper .media-grid.media-grid > a) {
+      display: block;
+      border: 1px solid;
+      width: calc(50% - 3px);
+      height: 500px;
+      position: relative;
+  }
+
+  :global(.cdu-service-mediagrid-wrapper .media-grid-element-title.media-grid-element-title) {
+      position: absolute;
+      bottom: 0;
+      color: #CCC;
+      z-index: 2;
+  }
+
+  :global(.cdu-service-mediagrid-wrapper .media-grid-element-source.media-grid-element-source) {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      overflow: hidden;
+      padding: 0;
+  }
+
+  :global(.cdu-service-mediagrid-wrapper .media-grid-element-source.media-grid-element-source img) {
+      min-width: 100%;
+      height: auto;
+      border-radius: 0;
+      width: 100%;
+  }
+</style>
