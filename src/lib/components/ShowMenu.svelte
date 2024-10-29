39e2418c8c879f240709f8f6f7eeab0c5d4d3b76
scott1223 <cdultra2012@gmail.com>
2024-10-29 20:06:06 +0100

    . fix bug

--- src/lib/components/ShowMenu.svelte
+++ src/lib/components/ShowMenu.svelte
@@ -6,14 +6,6 @@
 	function setBurger() {
         setTimeout( () => {
             document.querySelector('#header').classList.remove('invisible');
-            gsap.set('.header__menu li a span', {
-                yPercent: 100
-            });
-
-            gsap.set(header, {
-                width: '150px',
-                height: 'calc(28px + (2*10px))'
-            });
         }, 2000)
 	}
 	onMount(setBurger);
