d9d42daa4a813f5732c4d8b88c6d8e4178ba5cc5
scott1223 <cdultra2012@gmail.com>
2024-10-29 20:00:45 +0100

    Компонент принудительного отображения меню ShowMenu

---
+++ src/lib/components/ShowMenu.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { gsap } from 'gsap';
+    import { onMount } from 'svelte';
+
+    /* отображение меню / бургера */
+	function setBurger() {
+        setTimeout( () => {
+            document.querySelector('#header').classList.remove('invisible');
+            gsap.set('.header__menu li a span', {
+                yPercent: 100
+            });
+
+            gsap.set(header, {
+                width: '150px',
+                height: 'calc(28px + (2*10px))'
+            });
+        }, 2000)
+	}
+	onMount(setBurger);
+</script>
+
+<div class="d-none" />
