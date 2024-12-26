9ab26885ce8fb741134165b6607f3d00ade29bef
scott1223 <cdultra2012@gmail.com>
2024-12-26 14:59:35 +0100

    кейсы - сортировка

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -3,8 +3,14 @@ import { fetchWithRetry } from '$lib/utils.js';
 
 export async function load({ url }) {
 	try {
-		const casesURL = `${CMS_URL}/api/cases?fields[0]=title&populate[thumbnail]=true&populate[tags]=true&populate[SEO]=true&pagination[page]=1&pagination[pageSize]=6`;
+		const casesURL = `${CMS_URL}/api/cases?populate[thumbnail]=true&populate[tags]=true&populate[sort]=true&populate[SEO]=true&pagination[page]=1&pagination[pageSize]=100&sort[0]=sort:desc&sort[1]=id:desc`;
 		const casesData = await fetchWithRetry(casesURL);
+    casesData.data.sort((a, b) => {
+      const sortA = a.attributes.sort !== null ? a.attributes.sort : 0;
+      const sortB = b.attributes.sort !== null ? b.attributes.sort : 0;
+      return sortB - sortA || b.id - a.id;
+    });
+    casesData.data = casesData.data.slice(0,6);
 
 		const homeURL = `${CMS_URL}/api/home?populate[photoScroll][populate][images]=*&populate[workProccessStep][populate][images]=*&populate[heroImages1]=*&populate[heroImages2]=*&populate[SEO]=*`;
 		const homeData = await fetchWithRetry(homeURL);
--- src/routes/cases/+page.server.js
+++ src/routes/cases/+page.server.js
@@ -4,8 +4,14 @@ import { fetchWithRetry } from '$lib/utils.js';
 
 export async function load({ url }) {
 	try {
-		const casesURL = `${CMS_URL}/api/cases?fields[0]=title&populate[thumbnail]=true&populate[tags]=true&populate[SEO]=true&sort[0]=id:desc`;
+		const casesURL = `${CMS_URL}/api/cases?populate[thumbnail]=true&populate[tags]=true&populate[SEO]=true`;
 		const cases = await fetchWithRetry(casesURL);
+    
+    cases.data.sort((a, b) => {
+      const sortA = a.attributes.sort !== null ? a.attributes.sort : 0;
+      const sortB = b.attributes.sort !== null ? b.attributes.sort : 0;
+      return sortB - sortA || b.id - a.id;
+    })
 
 		const casesPageURL = `${CMS_URL}/api/cases-page?populate=*`;
 		const casesPage = await fetchWithRetry(casesPageURL);
